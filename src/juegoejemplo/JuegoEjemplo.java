af613bcc0bae7e50e591e01755f8d3fafa5c7d5a
LucasOsella <lucasosella01@gmail.com>
2024-03-19 11:52:36 -0300

    Agregue un nissan

--- src/juegoejemplo/JuegoEjemplo.java
+++ src/juegoejemplo/JuegoEjemplo.java
@@ -6,8 +6,12 @@ public class JuegoEjemplo {
     public static void main(String[] args) {
         // TODO code application logic here
         Auto ford = new Auto("rojo", 10,5);
+        Auto nissan =new Auto("Amarillo",15,20);
         Escena escena1=new Escena(ford);
+        Escena escena2=new Escena(nissan)
         escena1.jugar();
+        escena2.jugar();
+
       
       
         
